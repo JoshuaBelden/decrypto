14959cf15dca449ad2084461e918af54c7979f75
Joshua Belden <jcbelden@procaresoftware.com>
2025-03-22 20:18:18 -0600

    Added transition phase to main_encrypt

--- server/src/lib/game.js
+++ server/src/lib/game.js
@@ -1,10 +1,22 @@
 import { success, fail } from "./result.js"
+import { keywords } from '../data/keywords.js'
 
 const MIN_PLAYERS = 4
 export const GAME_PHASE = {
   LOBBY: "LOBBY",
-  STARTED: "STARTED",
-  FINISHED: "FINISHED",
+  MAIN_ENCRYPT: "MAIN_ENCRYPT",
+
+  MAIN_WHITE: "MAIN_WHITE",
+  MAIN_WHITE_INTERCEPT: "MAIN_WHITE_INTERCEPT",
+  MAIN_WHITE_DECODE: "MAIN_WHITE_DECODE",
+  MAIN_WHITE_REVEAL: "MAIN_WHITE_REVEAL",
+
+  MAIN_BLACK: "MAIN_BLACK",
+  MAIN_BLACK_INTERCEPT: "MAIN_BLACK_INTERCEPT",
+  MAIN_BLACK_DECODE: "MAIN_BLACK_DECODE",
+  MAIN_BLACK_REVEAL: "MAIN_BLACK_REVEAL",
+
+  OVER: "OVER",
 }
 
 const Player = (playerId, playerName, socket) => {
@@ -12,21 +24,18 @@ const Player = (playerId, playerName, socket) => {
   return { playerId, playerName, ready, socket }
 }
 
+const Team = (name) => {
+  const playerIds = []
+  const currentEncryptorId = null;
+  return { name, playerIds, currentEncryptorId }
+}
+
 const Game = gameId => {
   const state = {
-    phase: GAME_PHASE.LOBBY
+    phase: GAME_PHASE.LOBBY,
   }
   const players = []
-  const teams = [
-    {
-      name: "White",
-      playerIds: [],
-    },
-    {
-      name: "Black",
-      playerIds: [],
-    },
-  ]
+  const teams = [Team("White"), Team("Black")]
 
   const joinGame = (playerId, playerName, socket) => {
     if (players.some(player => player.playerId === playerId)) {
@@ -52,7 +61,7 @@ const Game = gameId => {
       return fail("Player is already on the team.")
     }
 
-    // Remove the player from all teams and then re-add them to the correct team 
+    // Remove the player from all teams and then re-add them to the correct team
     teams.forEach(team => {
       team.playerIds = team.playerIds.filter(id => id !== playerId)
     })
@@ -67,18 +76,84 @@ const Game = gameId => {
       return fail("Player is not in the game.")
     }
 
-    const onTeam = teams.find(team => team.playerIds.some(id => id === playerId))
+    const onTeam = teams.find(team =>
+      team.playerIds.some(id => id === playerId)
+    )
     if (!onTeam) {
       return fail("Player must be on a team to be ready.")
     }
 
     player.ready = ready
 
-    if (players.length >= MIN_PLAYERS && players.every(player => player.ready)) {
-      state.phase = GAME_PHASE.STARTED
-    }
+    transitionPhase()
 
     return success()
+  }
+
+  const setKeywords = () => {
+    const whiteTeam = teams.find(team => team.name === "White")
+    const blackTeam = teams.find(team => team.name === "Black")
+
+    whiteTeam.keywords = []
+    blackTeam.keywords = []
+
+    for (let i = 0; i < 4; i++) {
+      whiteTeam.keywords.push(keywords.splice(Math.floor(Math.random() * keywords.length), 1)[0])
+      blackTeam.keywords.push(keywords.splice(Math.floor(Math.random() * keywords.length), 1)[0])
+    }
+  }
+
+  const setNextEncryptors = () => {
+    const whiteTeam = teams.find(team => team.name === "White")
+    const blackTeam = teams.find(team => team.name === "Black")
+
+    whiteTeam.currentEncryptorId = whiteTeam.playerIds[0]
+    blackTeam.currentEncryptorId = blackTeam.playerIds[0]
+  }
+
+  const transitionPhase = () => {
+    switch (state.phase) {
+      case GAME_PHASE.LOBBY:
+        if (
+          players.length >= MIN_PLAYERS &&
+          players.every(player => player.ready)
+        ) {
+          setKeywords()
+          setNextEncryptors()
+          state.phase = GAME_PHASE.MAIN_ENCRYPT
+        }
+        break
+      case GAME_PHASE.MAIN_ENCRYPT:
+        state.phase = GAME_PHASE.MAIN_WHITE
+        break
+      case GAME_PHASE.MAIN_WHITE:
+        state.phase = GAME_PHASE.MAIN_WHITE_INTERCEPT
+        break
+      case GAME_PHASE.MAIN_WHITE_INTERCEPT:
+        state.phase = GAME_PHASE.MAIN_WHITE_DECODE
+        break
+      case GAME_PHASE.MAIN_WHITE_DECODE:
+        state.phase = GAME_PHASE.MAIN_WHITE_REVEAL
+        break
+      case GAME_PHASE.MAIN_WHITE_REVEAL:
+        state.phase = GAME_PHASE.MAIN_BLACK
+        break
+      case GAME_PHASE.MAIN_BLACK:
+        state.phase = GAME_PHASE.MAIN_BLACK_INTERCEPT
+        break
+      case GAME_PHASE.MAIN_BLACK_INTERCEPT:
+        state.phase = GAME_PHASE.MAIN_BLACK_DECODE
+        break
+      case GAME_PHASE.MAIN_BLACK_DECODE:
+        state.phase = GAME_PHASE.MAIN_BLACK_REVEAL
+        break
+      case GAME_PHASE.MAIN_BLACK_REVEAL:
+        state.phase = GAME_PHASE.OVER
+        break
+      case GAME_PHASE.OVER:
+        state.phase = GAME_PHASE.LOBBY
+        break
+    }
   }
 
   return {
--- server/src/test/game.test.js
+++ server/src/test/game.test.js
@@ -159,43 +159,123 @@ describe("Game", () => {
       // ASSERT
       expect(result.success).toBe(false)
     })
+  })
 
-    it("should advance the game phase when at least 4 players are all ready", () => {
-      // ARRANGE
-      const game = Game("game1")
-      const playerId1 = "player1"
-      const playerName1 = "Player One"
-      const socket1 = { send: jest.fn() }
-      game.joinGame(playerId1, playerName1, socket1)
-      game.joinTeam(playerId1, "White")
+  describe("transitionPhase", () => {
+    describe("lobby to main_encrypt", () => {
+      it("lobby should transition to main_encrypt", () => {
+        // ARRANGE
+        const game = Game("game1")
+        const playerId1 = "player1"
+        const playerName1 = "Player One"
+        const socket1 = { send: jest.fn() }
+        game.joinGame(playerId1, playerName1, socket1)
+        game.joinTeam(playerId1, "White")
 
-      const playerId2 = "player2"
-      const playerName2 = "Player Two"
-      const socket2 = { send: jest.fn() }
-      game.joinGame(playerId2, playerName2, socket2)
-      game.joinTeam(playerId2, "White")
+        const playerId2 = "player2"
+        const playerName2 = "Player Two"
+        const socket2 = { send: jest.fn() }
+        game.joinGame(playerId2, playerName2, socket2)
+        game.joinTeam(playerId2, "White")
 
-      const playerId3 = "player3"
-      const playerName3 = "Player Three"
-      const socket3 = { send: jest.fn() }
-      game.joinGame(playerId3, playerName3, socket3)
-      game.joinTeam(playerId3, "Black")
+        const playerId3 = "player3"
+        const playerName3 = "Player Three"
+        const socket3 = { send: jest.fn() }
+        game.joinGame(playerId3, playerName3, socket3)
+        game.joinTeam(playerId3, "Black")
 
-      const playerId4 = "player4"
-      const playerName4 = "Player Four"
-      const socket4 = { send: jest.fn() }
-      game.joinGame(playerId4, playerName4, socket4)
-      game.joinTeam(playerId4, "Black")
+        const playerId4 = "player4"
+        const playerName4 = "Player Four"
+        const socket4 = { send: jest.fn() }
+        game.joinGame(playerId4, playerName4, socket4)
+        game.joinTeam(playerId4, "Black")
 
-      // ACT
-      game.playerReady(playerId1, true)
-      game.playerReady(playerId2, true)
-      game.playerReady(playerId3, true)
-      const result = game.playerReady(playerId4, true)
+        // ACT
+        game.playerReady(playerId1, true)
+        game.playerReady(playerId2, true)
+        game.playerReady(playerId3, true)
+        const result = game.playerReady(playerId4, true)
 
-      // ASSERT
-      expect(result.success).toBe(true)
-      expect(game.state.phase).toBe(GAME_PHASE.STARTED)
+        // ASSERT
+        expect(result.success).toBe(true)
+        expect(game.state.phase).toBe(GAME_PHASE.MAIN_ENCRYPT)
+      })
+
+      it("main_encrypt should have 4 keywords set per team", () => {
+        // ARRANGE
+        const game = Game("game1")
+        const playerId1 = "player1"
+        const playerName1 = "Player One"
+        const socket1 = { send: jest.fn() }
+        game.joinGame(playerId1, playerName1, socket1)
+        game.joinTeam(playerId1, "White")
+
+        const playerId2 = "player2"
+        const playerName2 = "Player Two"
+        const socket2 = { send: jest.fn() }
+        game.joinGame(playerId2, playerName2, socket2)
+        game.joinTeam(playerId2, "White")
+
+        const playerId3 = "player3"
+        const playerName3 = "Player Three"
+        const socket3 = { send: jest.fn() }
+        game.joinGame(playerId3, playerName3, socket3)
+        game.joinTeam(playerId3, "Black")
+
+        const playerId4 = "player4"
+        const playerName4 = "Player Four"
+        const socket4 = { send: jest.fn() }
+        game.joinGame(playerId4, playerName4, socket4)
+        game.joinTeam(playerId4, "Black")
+
+        // ACT
+        game.playerReady(playerId1, true)
+        game.playerReady(playerId2, true)
+        game.playerReady(playerId3, true)
+        game.playerReady(playerId4, true)
+
+        // ASSERT
+        expect(game.teams[0].keywords).toHaveLength(4)
+        expect(game.teams[1].keywords).toHaveLength(4)
+      })
+
+      it("main_encrypt should choose 1 encryptor from each team", () => {
+        // ARRANGE
+        const game = Game("game1")
+        const playerId1 = "player1"
+        const playerName1 = "Player One"
+        const socket1 = { send: jest.fn() }
+        game.joinGame(playerId1, playerName1, socket1)
+        game.joinTeam(playerId1, "White")
+
+        const playerId2 = "player2"
+        const playerName2 = "Player Two"
+        const socket2 = { send: jest.fn() }
+        game.joinGame(playerId2, playerName2, socket2)
+        game.joinTeam(playerId2, "White")
+
+        const playerId3 = "player3"
+        const playerName3 = "Player Three"
+        const socket3 = { send: jest.fn() }
+        game.joinGame(playerId3, playerName3, socket3)
+        game.joinTeam(playerId3, "Black")
+
+        const playerId4 = "player4"
+        const playerName4 = "Player Four"
+        const socket4 = { send: jest.fn() }
+        game.joinGame(playerId4, playerName4, socket4)
+        game.joinTeam(playerId4, "Black")
+
+        // ACT
+        game.playerReady(playerId1, true)
+        game.playerReady(playerId2, true)
+        game.playerReady(playerId3, true)
+        game.playerReady(playerId4, true)
+
+        // ASSERT
+        expect(game.teams[0].currentEncryptorId).not.toBeNull()
+        expect(game.teams[1].currentEncryptorId).not.toBeNull()
+      })
     })
   })
 })
